0931bfd0af0ca843425926ef1eb5a423752e9e91
Kate-Baya <ksbaya@gmail.com>
2021-04-11 16:54:39 +1200

    added drag and drop

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 import BarChart from './BarChart'
 import QuantitativeTotal from './QuantitativeTotal'
 
-function Dashboard ({subSeries, data}) {
-  return(
+function Dashboard() {
+  const [state, setState] = useState({
+    components: [
+      { id: '1', name: BarChart},
+      { id: '2', name: QuantitativeTotal},
+    ]
+  })
+
+  function handleOnDragEnd(result) {
+    if(!result.destination ) return;
+    const items = Array.from(state.components);
+    const [reorderedItem] = items.splice(result.source.index, 1)
+    items.splice(result.destination.index, 0, reorderedItem)
+    
+    setState({components:items})
+  }
+
+  return (
     <>
-    <h1>Dashboard</h1>
-    <QuantitativeTotal />
-    <BarChart subSeries={subSeries}/>
+      <h1>Dashboard</h1>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId='components'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+            {state.components.map((e, idx) => {
+              return <Draggable key={e.id} draggableId={e.id} index={idx}>
+                {(provided) => (
+                  <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                  <e.name />
+                  </div>
+                )}
+                </Draggable>
+            })}
+            {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   )
 }
