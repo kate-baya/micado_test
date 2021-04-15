93bda0ee207f1bfb7b0227a44b54eecba43a940f
Kate-Baya <ksbaya@gmail.com>
2021-04-15 17:27:49 +1200

    subseries title loading dynamically

--- client/components/BarChart.jsx
+++ client/components/BarChart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { connect } from 'react-redux'
 import { scaleBand, scaleLinear, max, format } from 'd3'
 import { AxisBottom } from './barChartLayout/AxisBottom'
@@ -9,7 +9,8 @@ const margin = { top: 20, right: 30, bottom: 80, left: 120 }
 const xAxisLabelOffset = 65
 const xAxisTickFormat = format(".2s")
 
-function BarChart ({data, cat}) {
+function BarChart({ data }) {
+  const myRef = useRef(null)
 
   const parameterLength = []
   data.map(d => parameterLength.push(d.parameter))
@@ -19,30 +20,42 @@ function BarChart ({data, cat}) {
 
   const innerHeight = height - margin.top - margin.bottom
   const innerWidth = width - margin.left - margin.right
-  
+
   const yValue = d => d.parameter
   const xValue = d => d.value
+  
+  let xAxisLabel
+  if (data && data[0]) {
+    xAxisLabel = data[0].sub_series_name
+  }
 
   const yScale = scaleBand()
     .domain(data.map(yValue))
     .range([0, innerHeight])
     .paddingInner(0.5);
-  
+
   const xScale = scaleLinear()
     .domain([0, max(data, xValue)])
     .range([0, innerWidth]);
 
+  useEffect(() => {
+    myRef.current.scrollIntoView()
+  })
+
   return (
     <>
-    <h1>Data Visualisation</h1>
-    <svg width={width} height={height}>
-      <g transform={`translate(${margin.left},${margin.top})`}>
-        <AxisBottom xScale={xScale} innerHeight={innerHeight} tickFormat={xAxisTickFormat}/>
-        <AxisLeft yScale={yScale} />  
-        <text className='axis-label' x={innerWidth / 2} y={innerHeight + xAxisLabelOffset} textAnchor='middle'>{cat}</text>
-        <Marks data={data} xScale={xScale} yScale={yScale} xValue={xValue} yValue={yValue} tooltipFormat={xAxisTickFormat}/>
-      </g>
-    </svg>
+      <h1>Data Visualisation</h1>
+      <div className='fixedHeight'>
+        <svg width={width} height={height}>
+          <g transform={`translate(${margin.left},${margin.top})`}>
+            <AxisBottom xScale={xScale} innerHeight={innerHeight} tickFormat={xAxisTickFormat} />
+            <AxisLeft yScale={yScale} />
+            <Marks data={data} xScale={xScale} yScale={yScale} xValue={xValue} yValue={yValue} tooltipFormat={xAxisTickFormat} />
+            <text className='axis-label' x={innerWidth / 2} y={innerHeight + xAxisLabelOffset} textAnchor='middle'>{xAxisLabel}</text>
+          </g>
+        </svg>
+        <div ref={myRef}></div>
+      </div>
     </>
   )
 }
@@ -55,4 +68,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect (mapStateToProps)(BarChart)
+export default connect(mapStateToProps)(BarChart)
